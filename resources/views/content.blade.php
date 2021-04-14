a630ad7685a3f10e0a4074bffcb275ca660e3c5e
daigoroutaso <daigoroutaso@gmail.com>
2021-04-14 22:39:58 +0900

    aaa

--- resources/views/content.blade.php
+++ resources/views/content.blade.php
@@ -4,7 +4,7 @@
 <meta name="twitter:card" content="summary_large_image"></meta>
 <meta property="og:url" content="http://tubuyakiru.com/" />
 <meta property="og:title" content="麻雀つぶや切る" />
-<meta property="og:description" content="５秒で何切るを作って画像付きツイートできる麻雀学習ツールサービスです。" />
+<meta property="og:description" content="５秒で何切る問題を作ってURLを画像付きツイートできる麻雀学習ツールサービスです。" />
 <meta property="og:image" content="http://tubuyakiru.com/storage/twitter-card/{{$contents[0]->id}}.png" />
 @endsection
 
